7111d0cce42af082183b5d65b9603a259bd568c2
ricardov28 <40008886+ricardov28@users.noreply.github.com>
2023-05-14 22:43:08 -0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,36 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace MyNamespace
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
-            services.AddControllersWithViews();
-            services.AddSignalR();
+            Configuration = configuration;
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add SignalR hub as a service
+            services.AddSignalR();
+
+            // Add telemetry controller as a service
+            services.AddSingleton<TelemetryController>();
+
+            // Add MVC as a service and configure it to use the Razor view engine
+            services.AddMvc().AddRazorRuntimeCompilation();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -21,21 +38,22 @@ namespace MyNamespace
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
+            // Add routing middleware to the pipeline and configure it to use the default route
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHub<TelemetryHub>("/telemetryhub");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapHub<TelemetryHub>("/telemetryhub");
             });
         }
     }
